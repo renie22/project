051323550a6c710031ab436413dba8ef334690e0
Renie Adjani <renieadjani62@gmail.com>
2024-03-12 20:34:57 +0800

    update axios baseURL

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const newRequest = axios.create({
-  baseURL: "http://localhost:4600/api",
+  baseURL: "https://mern-server-fyvv.onrender.com/api",
   withCredentials: true,
 });
